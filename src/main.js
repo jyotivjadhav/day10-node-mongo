ab0fcc75817225d54875cab5058cdcdfc4787a77
Jyoti Jadhav <jyotivjadhav748@gmail.com>
2023-06-10 15:34:20 +0530

    hello-node-mongo

--- src/main.js
+++ src/main.js
@@ -1,2 +1,12 @@
-import {} from "node:fs";
-console.log("hello");
+import { MongoClient } from "mongodb";
+
+async function main(){
+    const uri="mongodb://127.0.0.1:27017";
+    const client = new MongoClient(uri);
+    const db=client.db("mydb");
+    const message=db.collection("message");
+    await message.insertOne({});
+    await client.close();
+    console.log("Record Added");
+}
+main();
