a2105774a5be377a425046a12c6dbaf795e44cf0
Jyoti Jadhav <jyotivjadhav748@gmail.com>
2023-06-10 15:56:03 +0530

    passing data as argument

--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
 import { MongoClient } from "mongodb";
 
-async function insertRecord(){
+async function insertRecord(jsonDocument){
     const uri="mongodb://127.0.0.1:27017";
     const client = new MongoClient(uri);
     const db=client.db("mydb");
     const message=db.collection("message");
-    let jsonDocument={message:"Hello World",sentBy:"jyoti",to:"jj"};
+    // let jsonDocument={message:"Hello World",sentBy:"jyoti",to:"jj"};
     await message.insertOne(jsonDocument);
     await client.close();
     console.log("Record Added");
 }
 async function main(){
-   await insertRecord();
+    let jsonDocument={message:"Hello universe   ",sentBy:"jyoti",to:"jj"};
+    await insertRecord(jsonDocument);
 }
 main();
