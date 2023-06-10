4a8986e5dc8993298586e02baf8de7d09a144ccd
Jyoti Jadhav <jyotivjadhav748@gmail.com>
2023-06-10 15:47:51 +0530

    branch2 seperate insert method

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import { MongoClient } from "mongodb";
 
-async function main(){
+async function insertRecord(){
     const uri="mongodb://127.0.0.1:27017";
     const client = new MongoClient(uri);
     const db=client.db("mydb");
     const message=db.collection("message");
-    await message.insertOne({});
+    let jsonDocument={message:"Hello World",sentBy:"jyoti",to:"jj"};
+    await message.insertOne(jsonDocument);
     await client.close();
     console.log("Record Added");
+}
+async function main(){
+   await insertRecord();
 }
 main();
